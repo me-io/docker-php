504a0d5bf01242a94961ff4211d49da11579dead
Mohamed Meabed <mo.meabed@gmail.com>
2018-08-13 22:29:26 +0400

    Update images

--- php56/build.sh
+++ php56/build.sh
@@ -9,17 +9,6 @@ AMQP_VERSION="1.9.3"
 
 DOCKER_TAG=${PHP_VERSION}
 
-docker build --build-arg PHP_VERSION=${PHP_VERSION} \
-             --build-arg REDIS_VERSION=${REDIS_VERSION} \
-             --build-arg MONGO_VERSION=${MONGO_VERSION} \
-             --build-arg AMQP_VERSION=${AMQP_VERSION} \
-             -t ${REPO_NAME}:${PHP_VERSION} ${DIR}
-
-if [[ $? != 0 ]]; then
-    echo "php ${PHP_VERSION} build failed"
-    exit 1
-fi
-
 if [[ ! -z "${DOCKER_PASSWORD}" && ! -z "${DOCKER_USERNAME}" ]]
 then
     echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
@@ -27,9 +16,24 @@ fi
 
 TAG_EXIST=`curl -s "https://hub.docker.com/v2/repositories/${REPO_NAME}/tags/${DOCKER_TAG}/" | grep '"id":'`
 
+if [[ ! -z ${TAG_EXIST}  ]]; then
+    echo "${REPO_NAME}/${DOCKER_TAG} already exist"
+    exit 0
+fi
+
+docker build --build-arg PHP_VERSION=${PHP_VERSION} \
+             --build-arg REDIS_VERSION=${REDIS_VERSION} \
+             --build-arg MONGO_VERSION=${MONGO_VERSION} \
+             --build-arg AMQP_VERSION=${AMQP_VERSION} \
+             -t ${REPO_NAME}:${PHP_VERSION} ${DIR}
+
+if [[ $? != 0 ]]; then
+    echo "${REPO_NAME}/${DOCKER_TAG} build failed"
+    exit 1
+fi
+
+
 if [[ -z ${TAG_EXIST}  ]]; then
     docker push ${REPO_NAME}:${PHP_VERSION}
-    echo "php ${PHP_VERSION} pushed successfully"
-else
-    echo "php ${PHP_VERSION} already exist"
+    echo "${REPO_NAME}/${DOCKER_TAG} pushed successfully"
 fi

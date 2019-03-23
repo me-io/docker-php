0385b1d4de025c49f1ccbf78ebf27201539a5416
Mohamed Meabed <mo.meabed@gmail.com>
2019-03-23 04:12:31 +0400

    update php and pkgs, drop php56 70 71, adding php73

--- php73/build.sh
+++ php73/build.sh
@@ -3,13 +3,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 REPO_NAME="meio/php"
 # http://php.net/downloads.php
-PHP_VERSION="7.0.31"
+PHP_VERSION="7.3.3"
 # https://pecl.php.net/package-changelog.php?package=redis
-REDIS_VERSION="4.1.1"
+REDIS_VERSION="4.3.0"
 # https://pecl.php.net/package-changelog.php?package=mongodb
-MONGO_VERSION="1.5.2"
+MONGO_VERSION="1.5.3"
 # https://pecl.php.net/package-changelog.php?package=amqp
-AMQP_VERSION="1.9.3"
+AMQP_VERSION="1.9.4"
 
 DOCKER_TAG=${PHP_VERSION}
 

322304cfe42c4762349151a531f41e7ad42d68cc
Mohamed Meabed <mo.meabed@gmail.com>
2019-05-15 11:20:24 +0400

    update

--- php71/build.sh
+++ php71/build.sh
@@ -40,9 +40,7 @@ if [[ $? != 0 ]]; then
 fi
 
 
-if [[ -z ${TAG_EXIST}  ]]; then
-    if [[ "${FORCE}" != "f" ]]; then
-        docker push ${REPO_NAME}:${PHP_VERSION}
-        echo "${REPO_NAME}:${DOCKER_TAG} pushed successfully"
-    fi
+if [[ -z ${TAG_EXIST} -o "${FORCE}" == "f" ]]; then
+    docker push ${REPO_NAME}:${PHP_VERSION}
+    echo "${REPO_NAME}:${DOCKER_TAG} pushed successfully"
 fi

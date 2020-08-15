a62d388b8c7f66551437fc0ae7523691f08b2e21
Mohamed Meabed <mo.meabed@gmail.com>
2020-08-15 23:03:34 +0400

    chore: update

--- php72/build.sh
+++ php72/build.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 REPO_NAME="meio/php"
 # http://php.net/downloads.php
-PHP_VERSION="7.2.32"
+PHP_VERSION="7.2.33"
 # https://pecl.php.net/package-changelog.php?package=redis
 REDIS_VERSION="5.3.1"
 # https://pecl.php.net/package-changelog.php?package=mongodb

9cc719c71e92658f2996082a4a6b254fdcc0a72c
Mohamed Meabed <mo.meabed@gmail.com>
2020-12-06 02:41:15 +0400

    chore: update

--- php74/build.sh
+++ php74/build.sh
@@ -3,11 +3,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 REPO_NAME="meio/php"
 # http://php.net/downloads.php
-PHP_VERSION="7.4.9"
+PHP_VERSION="7.4.13"
 # https://pecl.php.net/package-changelog.php?package=redis
-REDIS_VERSION="5.3.1"
+REDIS_VERSION="5.3.2"
 # https://pecl.php.net/package-changelog.php?package=mongodb
-MONGO_VERSION="1.8.0"
+MONGO_VERSION="1.9.0"
 # https://pecl.php.net/package-changelog.php?package=amqp
 AMQP_VERSION="1.10.2"
 

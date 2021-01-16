1b75e3b7c449621767b07db2fb2b7826b04300e1
Mohamed Meabed <mo.meabed@gmail.com>
2021-01-16 06:14:26 +0400

    chore: update

--- php74/build.sh
+++ php74/build.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 REPO_NAME="meio/php"
 # http://php.net/downloads.php
-PHP_VERSION="7.4.13"
+PHP_VERSION="7.4.14"
 # https://pecl.php.net/package-changelog.php?package=redis
 REDIS_VERSION="5.3.2"
 # https://pecl.php.net/package-changelog.php?package=mongodb

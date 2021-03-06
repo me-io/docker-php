99d403967511694a28b8556648fca6811ee89db5
Mohamed Meabed <mo.meabed@gmail.com>
2021-03-06 17:04:42 +0400

    chore: update

--- php80/build.sh
+++ php80/build.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 REPO_NAME="meio/php"
 # http://php.net/downloads.php
-PHP_VERSION="8.0.8"
+PHP_VERSION="8.0.3"
 # https://pecl.php.net/package-changelog.php?package=redis
 REDIS_VERSION="5.3.3"
 # https://pecl.php.net/package-changelog.php?package=mongodb
